6199f78eab26d3f779d99a71cf31fc81dc33e2d1
Scott Spendolini <scott@sumnertech.com>
2017-11-10 13:02:32 -0500

    Script change and SQL file for report column definition data

--- source/cfg/_ins_cfg.sql
+++ source/cfg/_ins_cfg.sql
@@ -7,6 +7,6 @@
 @cfg/sv_sec_colors_dml.sql
 @cfg/sv_sec_pref_defaults_dml.sql
 @cfg/sv_sec_snippets.sql "^admin_email_address"
---@cfg/sv_sec_info_fix_pdf.sql
 @cfg/sv_sec_css_data.sql
 @cfg/sv_sert_eval_job.sql
+@cfg/sv_sert_rpt_data.sql
